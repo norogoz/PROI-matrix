6cc5972575ad282b32af0a3dba5f71111b379f90
Grzegorz Rogoziński <31848132+unquadhexium@users.noreply.github.com>
2018-03-14 11:31:48 +0100

    Add files via upload

---
+++ matrix.h
@@ -0,0 +1,17 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#define SIZE 5
+
+
+class matrix
+{
+    public:
+        int arr[SIZE][SIZE];
+        matrix();
+        ~matrix();
+
+    private:
+};
+
+#endif // MATRIX_H
